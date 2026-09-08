ae03c9d15d3dc3ca2d878895cf06dbc45dbd62cb
Camille McCamey <jcmccamey@mail.lipscomb.edu>
2026-09-08 16:48:01 -0500

    Student code

--- EECE3624-Lab03-main/Lab03.asm
+++ EECE3624-Lab03-main/Lab03.asm
@@ -29,15 +29,17 @@
  * to make sure all of your branches work properly
  ***********************************************************************/
 
-; //Read sensor data from SRAM and store in registers R20 (Sensor1) and R21 (Sensor2)
+; //Naming sensors and threshold
 .equ THRESHOLD = 0x90 ; Create a constant
-.def Sensor1 = R20 ; Define a nickname for R20
-.def Sensor2 = R21 ; Define a nickname for R21
+.def Sensor1 = R20 ; Define a nickname for R20 (unsigned)
+.def Sensor2 = R21 ; Define a nickname for R21 (signed)
 
+; //Reset
 .org 0x0000 ; next instruction will be written to address 0x0000
             ; (the location of the reset vector)
 RJMP main	; set reset vector to point to the main code entry point
 
+; //Pointers initialization
 main:       ; jump here on reset
 
 	; initialize the stack (RAMEND = 0x10FF by default for the ATmega128A)
@@ -47,4 +49,53 @@ main:       ; jump here on reset
 	OUT SPL, R16
 
     ;----------------------------------
-    ; student-written code begins here    
+
+; //Loading sensor values (box 1)
+LDI YH, HIGH(0x0100)
+LDI YL, LOW(0x0100)
+LD Sensor1, Y+
+LD Sensor2, Y
+
+; //Sensor1 >= THRESHOLD y=0x46 at 0x0110, n=0x50 at 0x0110
+LDI YH, HIGH(0x0100)
+LDI YL, LOW(0x0100)	
+LDI R22, THRESHOLD
+CP Sensor1, R22
+BRSH Sensor1Y               ; branch yes
+
+; //Branches Sensor1
+Sensor1N: LDI R23, 0x50
+		  ST Y+, R23        ; Y points to 0x0111
+		  RJMP Sensor1Store ; skip branch yes
+Sensor1Y: LDI R23, 0x46
+		  ST Y+, R23        ; Y points to 0x0111
+
+; //Store Sensor1 in 0x0111
+Sensor1Store: ST Y+, Sensor1
+
+; //Sensor2 < THRESHOLD y==>i at 0x0112 n==>s at 0x0112
+LDI R22, THRESHOLD
+CP Sensor2, R22
+BRLT Sensor2Y               ; branch yes
+
+; //Branches Sensor2
+Sensor2N: LDI R23, 's'
+		  ST Y+, R23        ; Y points to 0x0113
+		  RJMP Sensor12Compare
+Sensor2Y: LDI R23, 'i'
+		  ST Y+, R23        ; Y points to 0x0113
+
+; //Sensor1 != Sensor2
+Sensor12Compare: CP Sensor1, Sensor2
+				 BRNE NotEqual
+
+; //Branches compare
+Equal: LDI R23, 0x08
+	   ST Y, R23
+	   RJMP Done
+NotEqual: LDI R23, 0x15
+		  ST Y, R23
+
+Done:
+nop
+
